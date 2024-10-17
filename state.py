2a17293c1e4553d9ba641438d53c12fea18f8652
Lostong <nazarvol323@gmail.com>
2024-10-18 00:18:06 +0300

    Delete state.py

--- state.py
+++
@@ -1,7 +0,0 @@
-from aiogram.fsm.context import FSMContext
-from aiogram.fsm.state import State, StatesGroup
-
-class MissionsFrom(StatesGroup):
-    name = State()
-    description = State()
-    how_to_achieve = State()
